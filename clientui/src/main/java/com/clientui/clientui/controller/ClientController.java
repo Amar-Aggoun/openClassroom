9e2714de4d494e21605ac41fd59e0da2fa44c8d3
Amar-Aggoun <amar.aggoun@univ-constantine2.dz>
2023-12-13 12:30:58 +0100

    added a detail page for each product - added method in client controller

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -4,6 +4,7 @@ import com.clientui.clientui.proxies.MicroserviceProduitsProxy;
 import com.clientui.clientui.beans.ProductBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
@@ -20,5 +21,13 @@ public class ClientController {
         List<ProductBean> produits =  produitsProxy.listeDesProduits();
         model.addAttribute("produits", produits);
         return "Accueil";
+    }
+
+    @RequestMapping("/details-produit/{id}")
+
+    public String ficheProduit(@PathVariable int id, Model model){
+        ProductBean produit = produitsProxy.recupererUnProduit(id);
+        model.addAttribute("produit", produit);
+        return "FicheProduit";
     }
 }
