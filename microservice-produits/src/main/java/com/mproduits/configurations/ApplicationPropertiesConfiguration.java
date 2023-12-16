4218955b6542c7d9aafcb9b0b9081ee6abb70be8
Amar-Aggoun <amar.aggoun@univ-constantine2.dz>
2023-12-16 18:38:15 +0100

    rest of project

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -1,25 +1,21 @@
 package com.mproduits.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
-
 
 @Component
 @ConfigurationProperties("mes-configs")
-public class ApplicationPropertiesConfiguration
-{
+public class ApplicationPropertiesConfiguration {
 
     private int limitDeProduits;
 
-    public int getLimitDeProduits()
-    {
-
+    public int getLimitDeProduits() {
         return limitDeProduits;
-
     }
 
-    public void setLimitDeProduits(int limitDeProduits)
-    {
+    public void setLimitDeProduits(int limitDeProduits) {
         this.limitDeProduits = limitDeProduits;
     }
 }
--- microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
+++ microservice-produits/src/main/java/com/mproduits/web/controller/ProductController.java
@@ -4,7 +4,12 @@ import com.mproduits.configurations.ApplicationPropertiesConfiguration;
 import com.mproduits.dao.ProductDao;
 import com.mproduits.model.Product;
 import com.mproduits.web.exceptions.ProductNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,29 +18,43 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class ProductController {
+public class ProductController implements HealthIndicator {
 
     @Autowired
-    private final ProductDao productDao;
-    private final ApplicationPropertiesConfiguration appProperties;
+    ProductDao productDao;
 
-    public ProductController(ProductDao productDao, ApplicationPropertiesConfiguration appProperties){
-        this.productDao = productDao;
-        this.appProperties = appProperties;
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    ApplicationPropertiesConfiguration appProperties;
+
+    @Override
+    public Health health() {
+
+        List<Product> products = productDao.findAll();
+
+        if(products.isEmpty()) {
+            return Health.down().build();
+        }
+        return Health.up().build();
     }
-
 
     // Affiche la liste de tous les produits disponibles
     @GetMapping(value = "/Produits")
-    public List<Product> listeDesProduits()
-    {
+    public List<Product> listeDesProduits(){
+
         List<Product> products = productDao.findAll();
 
         if(products.isEmpty()) throw new ProductNotFoundException("Aucun produit n'est disponible à la vente");
 
         List<Product> listeLimitee = products.subList(0, appProperties.getLimitDeProduits());
 
+
+        log.info("Récupération de la liste des produits");
+
         return listeLimitee;
+
     }
 
     //Récuperer un produit par son id
@@ -48,6 +67,5 @@ public class ProductController {
 
         return product;
     }
-
 }
 
